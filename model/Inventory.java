e6cce662c2774ab179ae6f7d9f46e1d82c04e94a
Jon Waters <waters1228@gmail.com>
2020-11-18 10:43:14 -0500

    Add class Inventory.java

---
+++ model/Inventory.java
@@ -0,0 +1,105 @@
+package model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Inventory {
+
+    private static ObservableList<Part> allParts;
+
+    private static ObservableList<Product> allProducts;
+
+    public static ObservableList<Part> getAllParts() {
+        return allParts;
+    }
+
+    public static ObservableList<Product> getAllProducts() {
+        return allProducts;
+    }
+
+    public static void addPart(Part newPart) {
+        allParts.add(newPart);
+    }
+
+    public static void addProduct(Product newProduct) {
+        allProducts.add(newProduct);
+    }
+
+    public static Part lookupPart(int partId) {
+        Part partFound = null;
+        
+        for (Part part : allParts) {
+            if (part.getId() == partId) {
+                partFound = part;
+            }
+        }
+        
+        return partFound;
+    }
+
+    public static ObservableList<Part> lookupPart(String partName) {
+        ObservableList<Part> partFound = FXCollections.observableArrayList();
+
+        for (Part part : allParts) {
+            if (part.getName().equals(partName)) {
+                partFound.add(part);
+            }
+        }
+
+        return partFound;
+    }
+
+    public static Product lookupProduct(int productId) {
+        Product productFound = null;
+
+        for (Product product : allProducts) {
+            if (product.getId() == productId) {
+                productFound = product;
+            }
+        }
+
+        return productFound;
+    }
+
+    public static ObservableList<Product> lookupProduct(String productName) {
+        ObservableList<Product> productFound = FXCollections.observableArrayList();
+
+        for (Product product : allProducts) {
+            if (product.getName().equals(productName)) {
+                productFound.add(product);
+            }
+        }
+
+        return productFound;
+    }
+
+    public static void updatePart (int index, Part selectedPart) {
+
+        allParts.set(index, selectedPart);
+    }
+
+    public static void updateProduct (int index, Product selectedProduct) {
+
+        allProducts.set(index, selectedProduct);
+    }
+
+    public static boolean deletePart(Part selectedPart) {
+        if (allParts.contains(selectedPart)) {
+            allParts.remove(selectedPart);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static boolean deleteProduct(Product selectedProduct) {
+        if (allProducts.contains(selectedProduct)) {
+            allProducts.remove(selectedProduct);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
