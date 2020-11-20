add0a394a8b0ba0cbd272dba95316529cad8f77d
Jon Waters <waters1228@gmail.com>
2020-11-20 10:51:24 -0500

    Add more sample data to Main

--- main/Main.java
+++ main/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.InHouse;
+import model.Inventory;
 import model.Outsourced;
 import model.Product;
 
@@ -22,6 +23,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
+        //Add sample parts
         InHouse tvScreen = new InHouse(1,"TV Screen", 300.00, 5, 1, 20,
                 101);
         InHouse tvShell = new InHouse(2,"TV Shell", 100.00, 5, 1, 20,
@@ -30,13 +32,19 @@ public class Main extends Application {
                 101);
         Outsourced remote = new Outsourced(4, "Remote Control",29.99, 50, 30,
                 100, "Panasonic");
+        Inventory.addPart(tvScreen);
+        Inventory.addPart(tvShell);
+        Inventory.addPart(powerCord);
+        Inventory.addPart(remote);
 
+        //Add sample product
         Product television = new Product(1, "LCD Television", 499.99, 20, 20,
                 100);
         television.addAssociatedPart(tvScreen);
         television.addAssociatedPart(tvShell);
         television.addAssociatedPart(powerCord);
         television.addAssociatedPart(remote);
+        Inventory.addProduct(television);
 
         launch(args);
     }
