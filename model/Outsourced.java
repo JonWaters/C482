e1b0fe426790e63654e7381f10c0c27bb8562da9
Jon Waters <waters1228@gmail.com>
2020-11-25 09:03:18 -0500

    Correct issue with constructor missing parameter

--- model/Outsourced.java
+++ model/Outsourced.java
@@ -6,6 +6,7 @@ public class Outsourced extends Part {
 
     public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName) {
         super(id, name, price, stock, min, max);
+        this.companyName = companyName;
     }
 
     public String getCompanyName() {
